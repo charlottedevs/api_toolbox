6c0618f97e3d3d0ff2e09471c178c9e7bb359927
John Mason <john@johnmason.io>
2017-04-07 10:07:42 -0400

    update nil check

--- lib/api_toolbox/fetch_user.rb
+++ lib/api_toolbox/fetch_user.rb
@@ -6,7 +6,7 @@ module ApiToolbox
       HTTParty.get(users_url, query: user_params).tap do |res|
         context.fail!(errors: res.body) unless res.success?
         parsed_response = JSON.parse(res.body)
-        context.fail!(errors: { response: "no content" }) if parsed_response.empty?
+        context.fail!(errors: { response: "no content" }) if parsed_response.nil? || parsed_response.empty?
         context.user = parsed_response
       end
     end
